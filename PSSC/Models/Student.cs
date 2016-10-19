cf6cf1adc63ecf6cd14d0071406e7f07e600242e
MiriamGhioc <luana.miriam@yahoo.com>
2016-10-19 23:18:52 +0300

    Clase
    Clase

---
+++ PSSC/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public enum discipline{distribuita, examen};
+    class Student
+    {
+        public string nume_student;
+        public int an_studiu;
+        public int nota;
+        public int nota_marire;
+    }
+}
